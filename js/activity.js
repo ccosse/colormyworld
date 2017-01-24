feddb011cbebb8451f3a6d10d2cb2db1cf57e5d5
Charles Cosse <ccosse@gmail.com>
2017-01-24 02:56:28 -0700

    l10n title

--- js/activity.js
+++ js/activity.js
@@ -3,9 +3,10 @@ define([
 		"messages","print",
 		"activity/jquery-1.11.2.min",
 		"activity/ol",
-		"config","colormyworld","map",
+		"activity/l10n",
+		"config","colormyworld","map","util"
 	],
-	function (activity,messages,print,jquery,ol,config,colormyworld,map){
+	function (activity,messages,print,jquery,ol,l10n,config,colormyworld,map,util){
 
 	// Manipulate the DOM only when it is ready.
 	require(['domReady!'], function (doc) {
@@ -17,7 +18,21 @@ define([
 		map.setup_map();
 		colormyworld.change_areaCB();
 
-//		document.getElementById("cmw_bg").innerHTML="<h1>ColorMyWorld</h1>";
+		document.webL10n.setLanguage('fr');
+		print(document.webL10n.getLanguage());
+
+		var updateTitle=window.onresize=function(){
+			var app_title=String.split(document.webL10n.get('appname'),'');
+			var persistent_title_div=document.getElementById("persistent_title_div");
+			html="";
+			for(var tidx=0;tidx<app_title.length;tidx++){
+				var rand_color=util.mkBrightRGBA();
+				html+="<span style='text-shadow:none;font-family:Mickey;color:"+rand_color+";'>"+app_title[tidx]+"</span>";
+			}
+			persistent_title_div.innerHTML=html;
+			util.resize();
+		}
+		window.setTimeout(updateTitle,1000);
 
 		var myButton = document.getElementById("my-button");
 		myButton.onclick = function () {
